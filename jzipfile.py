7012e1b938f9a22e9b17f6bbb10133eaca674b49
Alex Sandro <Alex Sandro@FireDragon>
2013-03-11 21:37:46 -0300

    adicionando script usado na criação de um zip de uma extenção joomla.

---
+++ jzipfile.py
@@ -0,0 +1,14 @@
+import zipfile, os
+
+name = "com_pbevents_0.3"
+path = os.path.join(os.getcwd(), name)
+
+zip = zipfile.ZipFile(name+".zip", "w")
+
+for root, dirs, files in os.walk(path):
+    for file in files:
+        path = os.path.join(root, file)
+        path = path.replace(os.getcwd()+os.sep,"")
+        zip.write(path)
+        
+zip.close()
