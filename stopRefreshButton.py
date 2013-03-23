228f976f0c72fbc053bbf1fefb867bda179f0774
Alex Sandro <geniofuturo@gmail.com>
2013-03-23 20:46:08 -0300

    adicionando script usado pelo navegador.

---
+++ stopRefreshButton.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+from PySide import QtGui
+import os
+
+class StopRefreshButton(QtGui.QPushButton):
+    def __init__(self, *arg):
+        super(StopRefreshButton, self).__init__()
+        self.btnState = {}
+        self.setRefreshState()
+        
+    def setRefreshState(self):
+        path = os.path.join("media", "refresh.png")
+        self.setIcon(QtGui.QIcon(path))
+        self.btnState["state"] = "refresh"
+        self.setToolTip(self.tr("<b>reload</b>"))
+        
+    def setStopState(self):
+        qicon = QtGui.QIcon(os.path.join("media", "btnstop-blue.png"))
+        self.setIcon( qicon )
+        self.btnState["state"] = "stop"
+        self.setToolTip(self.tr("<b>stop</b>"))
+        
+    def __getitem__(self, key):
+        return self.btnState[key]
